41a32906d62551dbdb120564f33c19b133159f84
manassuri7 <49301320+manassuri7@users.noreply.github.com>
2019-12-29 19:33:39 +0530

    Update LoginBox.js

--- Basic/LoginBox.js
+++ Basic/LoginBox.js
@@ -2,12 +2,76 @@ export default class LoginBox extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      username:"",
+      password:"",
+         errors:[]
+    };
   }
 
-  submitLogin(e) {}
+  showValidationErr(elm, msg) {
+  this.setState((prevState) => ({
+    errors: [
+      ...prevState.errors, {
+        elm,
+        msg
+      }
+    ]
+  }));
+}
+  
+//Remove a specific element from the array 
+clearValidationErr(elm) {
+  this.setState((prevState) => {
+    let newArr = [];
+    //Add all elements from the prev array to the new one that has a different element
+    for (let err of prevState.errors) {
+      if (elm != err.elm) {
+        newArr.push(err);
+      }
+    }
+    return {errors: newArr};
+  });
+}
+  
+  onUsernameChange(e) {
+  this.setState({username: e.target.value});
+  //We want to clear the error when ever the user type something new 
+  this.clearValidationErr("username");
+}
+
+onPasswordChange(e) {
+  this.setState({password: e.target.value});
+  this.clearValidationErr("password");
+}
+  
+  submitLogin(e) {
+   //Check for all input fields and show errors if empty (you can implement other cases!)
+  if (this.state.username == "") {
+    this.showValidationErr("username", "Username Cannot be empty!");
+  }
+  if (this.state.email == "") {
+    this.showValidationErr("email", "Email Cannot be empty!");
+  }
+  }
 
   render() {
+    
+    let usernameErr = null,
+      passwordErr = null,
+
+    //Loop and find which ones has the error
+    for (let err of this.state.errors) {
+      //Assign the validation error message 
+      if (err.elm == "username") {
+        usernameErr = err.msg;
+      }
+      if (err.elm == "password") {
+        passwordErr = err.msg;
+      }
+      //No (else if or else) statements cause we need to check for all possible elements
+    }
+    
     return (
       <div className="inner-container">
         <div className="header">
@@ -21,7 +85,10 @@ export default class LoginBox extends React.Component {
               type="text"
               name="username"
               className="login-input"
-              placeholder="Username"/>
+              placeholder="Username"
+              onChange={this.onUsernameChange.bind(this)}/>
+              <small className="danger-error">{usernameErr
+                ? usernameErr : ""}
           </div>
 
           <div className="input-group">
@@ -30,7 +97,10 @@ export default class LoginBox extends React.Component {
               type="password"
               name="password"
               className="login-input"
-              placeholder="Password"/>
+              placeholder="Password"
+              onChange={this.onPasswordChange.bind(this)}/>
+              <small className="danger-error">{passwordErr
+                ? passwordErr : ""}
           </div>
 
           <button
